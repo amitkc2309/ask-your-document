2809429e4e79a648ce00eec0de8ee76315c1b6cf
Amit Chaudhary <amitkc2309@gmail.com>
2026-09-13 19:44:32 +0530

    Docker working

--- frontend/ask-your-document/src/config/config.ts
+++ frontend/ask-your-document/src/config/config.ts
@@ -1,6 +1,6 @@
 const config = {
-  apiUrl: import.meta.env.BACKEND_API_URL || 'http://localhost:8080',
-  keycloakUrl: import.meta.env.KEYCLOAK_URL || 'http://localhost:7080',
+  apiUrl: import.meta.env.VITE_BACKEND_API_URL || 'http://localhost:8080',
+  keycloakUrl: import.meta.env.VITE_KEYCLOAK_URL || 'http://localhost:7080',
 };
 
-export default config; 
+export default config;
